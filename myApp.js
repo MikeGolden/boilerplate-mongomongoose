44ff9b99a650be330c8ba8d3eda11a947f7152eb
Mike Goldenberg <csrutil@tuta.io>
2023-08-22 17:50:37 +0200

    connecting to mongo

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 
+/** # MONGOOSE SETUP #
+/*  ================== */
+
+/** 1) Install & Set up mongoose */
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 

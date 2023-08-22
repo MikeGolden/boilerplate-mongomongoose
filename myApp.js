aefe2c927588eb65d3a98a237b147acb8c029b47
Mike Goldenberg <csrutil@tuta.io>
2023-08-22 18:06:43 +0200

    utilising find methods

--- myApp.js
+++ myApp.js
@@ -40,12 +40,18 @@ var createManyPeople = function(arrayOfPeople, done) {
   });
 };
 
-const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+var findPeopleByName = function(personName, done) {
+  Person.find({name: personName}, function (err, personFound) {
+    if (err) return console.log(err);
+    done(null, personFound);
+  });
 };
 
-const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+var findOneByFood = function(food, done) {
+  Person.findOne({favoriteFoods: food}, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
